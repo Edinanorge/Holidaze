be3316f18e109a0755afdb9a16d641f4acfded5f
Edinanorge <52853951+Edinanorge@users.noreply.github.com>
2023-11-27 13:57:58 +0100

    Adds react toaster

--- src/AppLayout.tsx
+++ src/AppLayout.tsx
@@ -2,6 +2,7 @@ import { Outlet } from "react-router-dom";
 import Footer from "./ui/Footer";
 import Header from "./ui/Header/Header";
 import { SearchProvider } from "./context/searchContext";
+import { Toaster } from "react-hot-toast";
 
 function AppLayout() {
   return (
@@ -9,6 +10,7 @@ function AppLayout() {
       <Header />
       <Outlet />
       <Footer />
+      <Toaster position="top-center" reverseOrder={false} />
     </SearchProvider>
   );
 }
